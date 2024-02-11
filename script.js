33a4a96d87323f3b4070fd0262253a4425d329d1
mahadk28 <159662805+mahadk28@users.noreply.github.com>
2024-02-10 23:11:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
-document.getElementById('yesButton').addEventListener('click', function() {
-    document.getElementById('question').style.display = 'none';
-    document.getElementById('loveParagraph').style.display = 'block';
-});
+body, html {
+    height: 100%;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-family: Arial, sans-serif;
+}
 
-document.getElementById('noButton').addEventListener('click', function() {
-    alert('Maybe you clicked by mistake? Feel free to try again!');
-});
+.screen {
+    display: none;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+
+#memoryGameContainer {
+    display: flex;
+    flex-wrap: wrap;
+    width: 400px;
+    justify-content: space-around;
+}
+
+.memory-card {
+    width: 90px;
+    height: 90px;
+    margin: 10px;
+    background-color: #f3f3f3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 50px;
+    cursor: pointer;
+    border: 2px solid #fff;
+    border-radius: 10px;
+}
+
+.active {
+    display: flex;
+}
